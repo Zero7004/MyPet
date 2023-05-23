202b20ffa376951e6b7a13951cca68d4b9463872
wangjianlong <wangjianlong@formssi.com>
2023-05-23 15:18:47 +0800

    添加文件

---
+++ MyPet/Classes/Home/ViewController/MPGetPetViewController.h
@@ -0,0 +1,17 @@
+//
+//  MPGetPetViewController.h
+//  MyPet
+//
+//  Created by long on 2023/5/23.
+//  Copyright © 2023 王健龙. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPGetPetViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyPet/Classes/Home/ViewController/MPGetPetViewController.m
@@ -0,0 +1,24 @@
+//
+//  MPGetPetViewController.m
+//  MyPet
+//
+//  Created by long on 2023/5/23.
+//  Copyright © 2023 王健龙. All rights reserved.
+//
+
+#import "MPGetPetViewController.h"
+
+@interface MPGetPetViewController ()
+
+@end
+
+@implementation MPGetPetViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+@end
