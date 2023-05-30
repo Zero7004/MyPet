fc44f9060912332535f3bfe153c85e48875e0e51
wangjianlong <>
2023-05-30 23:08:46 +0800

    添加文件

---
+++ MyPet/Classes/Home/CustomUI/MPFuncCollectionView/MPFuncCollectionView.h
@@ -0,0 +1,17 @@
+//
+//  MPFuncCollectionView.h
+//  MyPet
+//
+//  Created by long on 2023/5/30.
+//  Copyright © 2023 王健龙. All rights reserved.
+//
+
+#import "BaseView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPFuncCollectionView : BaseView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyPet/Classes/Home/CustomUI/MPFuncCollectionView/MPFuncCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MPFuncCollectionViewCell.h
+//  MyPet
+//
+//  Created by long on 2023/5/30.
+//  Copyright © 2023 王健龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MPFuncCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
